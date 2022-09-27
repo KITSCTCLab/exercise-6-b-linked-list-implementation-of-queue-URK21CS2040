186b9e762def81557231a0eae8c8dfabe5f2424d
URK21CS2040 <110082089+URK21CS2040@users.noreply.github.com>
2022-09-27 23:22:09 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -7,39 +7,41 @@ class Node:
 class Queue:
   def __init__(self):
     self.head = None
-    self.tail = None
+    self.last = None
 
   def enqueue(self, data) -> None:
     # Write your code here
-    new = Node(data)
-    if not self.tail is None:
-      self.tail.next = new
-    if self.head is None:
-      self.head = new
-    self.tail = new
-    
+    if self.last is None:
+      self.head = Node(data)
+      self.last = self.head
+    else:
+      self.last.next = Node(data)
+      self.last = self.last.next
 
   def dequeue(self) -> None:
     # Write your code here
-    if not self.head is None:
+    if self.head is None:
+      return None
+    else:
+      val_returned = self.head.data
       self.head = self.head.next
-      if self.head is None:
-        self.tail = None
-
 
   def status(self) -> None:
     # Write your code here
-    elements = ""
-    curr = self.head
-    while not curr is None:
-      elements += str(curr.data) + "=>"
-      curr = curr.next
-    print(elements + "None")
+    current = self.head
+    status_of_queue = []
+    while (current):
+      status_of_queue.append(current.data)
+      current = current.next
+    for element in status_of_queue:
+      print(element, end="=>")
+    print(None)
 
 
 # Do not change the following code
 queue = Queue()
 operations = []
+
 for specific_operation in input().split(','):
     operations.append(specific_operation.strip())
 input_data = input()
